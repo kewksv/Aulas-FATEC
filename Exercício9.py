b1a6fa70f8d1635e1f930c7d89b3b0fe6eb0eca8
Gabriel Guirra "Kewksv <gabrielguirra67@gmail.com>
2026-08-26 13:47:22 -0300

    Delete Exercício9.py

--- Exercício9.py
+++
@@ -1,4 +0,0 @@
-# Ex2. Sensor de Temperatura de Servidor
-temperatura = input("Digite a temperatura atual em Celsius: ")
-alerta_ativo = (float(temperatura)) > 40
-print(f"Alerta de superaquecimento ativo? {({False:'Não', True:'Sim'})[alerta_ativo]}")
