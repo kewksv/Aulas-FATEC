0c61f0475c11c88d96b05f14d057e11fcf6467c5
Gabriel Guirra "Kewksv <gabrielguirra67@gmail.com>
2026-09-09 15:41:03 -0300

    Revise volleyball selection logic and messages
    Updated the program to classify participants based on height for volleyball selection.

--- Exercício18.py
+++ Exercício18.py
@@ -1,12 +1,23 @@
+# 5. Peneira do Vôlei (Condicional Simples if/else)
+# O treinador do time de vôlei da escola estipulou que, para participar da seleção masculina/feminina,
+# o candidato deve medir pelo menos 1.70m de altura.
+# Escreva um programa que peça a altura do aluno e exiba se ele está
+# "Aprovado para o teste" ou "Classificado para outras modalidades".
+# Uso do operador relacional de maior ou igual (>=) e a conversão da entrada para número real (float).
+
 import time
-print ("Bem vindo a paneira do vôlei!")
-time.sleep (0.5)
+
+print("Bem vindo a peneira do vôlei!")
+time.sleep(0.5)
+
 nome = input("Informe seu nome por gentileza: ")
-altura =  input("Digite a sua altura (em metros): ").replace(",", ".")
-if (float(altura)) >= 1.70:
-    time.sleep (1)
-    print(f"{nome} liberado para jogar vôlei!")
+altura = input("Digite a sua altura (em metros): ").replace(",", ".")
+
+if float(altura) >= 1.70:  # VERIFICA SE A ALTURA É SUFICIENTE
+    time.sleep(1)
+    print(f"{nome}, Aprovado para o teste!")
 else:
-    time.sleep (1)
-    print(f"{nome} não liberado para jogar vôlei!")
+    time.sleep(1)
+    print(f"{nome}, Classificado para outras modalidades!")
+
 print("Obrigado por participar do programa! :)")
