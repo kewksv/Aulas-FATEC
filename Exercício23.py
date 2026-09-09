dc9eadc53a388f23b620748fa446e86d9d141e4b
Gabriel Guirra "Kewksv <gabrielguirra67@gmail.com>
2026-09-09 15:43:27 -0300

    Enhance Exercício23.py with comments
    Added comments to clarify discount calculation logic.

--- Exercício23.py
+++ Exercício23.py
@@ -1,13 +1,22 @@
+# 10. Cálculo de Desconto (Aritmética e Decisão)
+# Peça o valor de um produto (real).
+# Se o produto custar mais de R$ 100.00, aplique um desconto de 10% e mostre o preço final.
+# Senão, exiba o preço original.
+
 import time
+
 print("Bem-vindo ao programa de desconto!")
-time.sleep (0.5)
+time.sleep(0.5)
+
 produto = float(input("Digite o valor do produto: R$"))
+
 if produto > 100:
-    desconto = produto * 0.10
-    produto_final = produto - desconto
+    desconto = produto * 0.10  # CALCULA 10% DE DESCONTO
+    produto_final = produto - desconto  # SUBTRAI O DESCONTO DO PREÇO ORIGINAL
     print(f"O valor do produto com desconto é: R${produto_final:.2f}")
     time.sleep(1)
 else:
     time.sleep(1)
     print(f"O valor do produto é: R${produto:.2f}")
-print ("Obrigado por participar do programa! :)")
+
+print("Obrigado por participar do programa! :)")
